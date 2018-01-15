fa776efda9e53a5ec5126be46958707c6015d8a9
Crystal Williams <cryswill10@gmail.com>
2018-01-15 08:50:18 -0600

    created mobile page and linked to main page.

--- assets/js/time-selector.js
+++ assets/js/time-selector.js
@@ -4,20 +4,31 @@ window.addEventListener('touchstart', function onFirstTouch() {
   window.removeEventListener('touchstart', onFirstTouch, false);
 }, false);
 
-var x = {
-  document.getElementsByClass("time checkbox");
-  x.ontouchstart = openMenu() {
-    alert("This is my menu.");
-  }
+// The menu should give the user the ability to select a start time, an end time,
+var check = document.getElementsById("time checkbox");
+check.addEventListener('touchstart', timeSelector);
+
+function timeSelector() {
+  var start =
+  prompt("Please type an start time:");
+  alert(start);
+
+  var end =
+  prompt("Please type an end time");
+  alert(end);
+
 }
 
-// The menu should give the user the ability to select a start time, an end time,
 
 // and when the user is done with the menu, the checkboxes in the calendar should be selected based on the user's inputs.
 
+function showSelection() {
+
+}
+
 // Users should be able to repeat this process to select as many blocks of checkboxes as they want.
 
-
+// if touchstart, then run functions, if not, stop.
 
 
 // While adding this functionality, make sure to follow progressive enhancement and that none of the existing functionalities are broken.
