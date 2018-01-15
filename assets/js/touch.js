724a8195cf7ed0828c2c11368db2a9dca9fd3f45
Crystal Williams <cryswill10@gmail.com>
2018-01-14 18:16:06 -0600

    add touch device conditional and set up alert for user tap.

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -1,19 +1,27 @@
 // is the device mobile?
-function IsTouchDevice() {
-  return 'ontouchstart' in window;
+function isTouchDevice() {
+  return 'ontouchstart' in document.documentElement;
 }
 
+if (isTouchDevice) {
+  document.documentElement.className -= "no-touch";
+}
+
+
 // when checkbox is tapped, open a menu:
+var calendar = body.getElementById("calendar");
+calendar.ontouchstart = timeSelectorMenu();
 
 // includes the following:
-function TimeSelectorMenu() {
+function timeSelectorMenu() {
+  alert("hello world");
 }
 
 // the ability to select a start time and an end time
 
 // based on the selections, all boxes are selected
 
-function TimeSelectorHighlight() {
+function timeSelectorHighlight() {
 
 }
 // ability to repeat the process (loop?)
