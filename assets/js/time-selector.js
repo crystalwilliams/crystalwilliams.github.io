fe00dae2947b18274f68d2b4b09ac997676c7e72
Crystal Williams <cryswill10@gmail.com>
2018-01-14 18:52:08 -0600

    fixed touch detectionand created alert for testing user tap.

---
+++ assets/js/time-selector.js
@@ -0,0 +1,31 @@
+jQuery( document ).ready(function( $ ) {
+
+  // Touch Device Detection
+  var isTouchDevice = 'ontouchstart' in document.documentElement;
+  if( isTouchDevice ) {
+    $('body').removeClass('no-touch');
+  }
+
+});
+
+// when checkbox is tapped, open a menu:
+var calendar = document.getElementByClass("time checkbox");
+calendar.ontouchstart = timeSelectorMenu();
+function timeSelectorMenu() {
+  alert("hello world");
+}
+
+// includes the following:
+// function timeSelectorMenu() {
+//   alert("hello world");
+// }
+
+// the ability to select a start time and an end time
+
+// based on the selections, all boxes are selected
+
+// function timeSelectorHighlight() {
+
+}
+// ability to repeat the process (loop?)
+
