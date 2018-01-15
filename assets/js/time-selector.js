a78201bab61a5a6d9b39b0c316fcec9d41285b35
Crystal Williams <cryswill10@gmail.com>
2018-01-14 20:07:48 -0600

    downloaded modernizr

--- assets/js/time-selector.js
+++ assets/js/time-selector.js
@@ -1,9 +1,20 @@
+jQuery( document ).ready(function( $ ) {
+  // Inside of this function, $() will work as an alias for jQuery()
+  // and other libraries also using $ will not be accessible under this shortcut
+  // https://codex.wordpress.org/Function_Reference/wp_enqueue_script#jQuery_noConflict_Wrappers
 
   // Touch Device Detection
-  var isTouchDevice = "ontouchstart" in document.documentElement;
+  var isTouchDevice = 'ontouchstart' in document.documentElement;
   if( isTouchDevice ) {
-    document.documentElement.removeClass("no-touch");
-}
+    $('body').removeClass('no-touch');
+  }
+
+// });
+//   // Touch Device Detection
+//   var isTouchDevice = "ontouchstart" in document.documentElement;
+//   if( isTouchDevice ) {
+//     document.documentElement.removeClass("no-touch");
+// }
 
 // when checkbox is tapped, open a menu:
 var calendar = document.getElementByClass("time checkbox");
@@ -14,9 +25,10 @@ function timeSelectorMenu() {
 
 
 // includes the following:
-// function timeSelectorMenu() {
-//   alert("hello world");
-// }
+
+function timeSelectorMenu() {
+  alert("hello world");
+}
 
 // the ability to select a start time and an end time
 
